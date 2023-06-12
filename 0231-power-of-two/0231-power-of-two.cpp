94f1cf82d864c378ce5f25ae5f43e36c8b50fc8f
architnangla7 <136116152+architnangla7@users.noreply.github.com>
2023-06-13 00:21:04 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (15.86%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+    /*Brute Force
        for(int i=0;i<=30;i++)
        {
            int ans=pow(2,i);
@@ -11,5 +12,20 @@ public:
            }
        }
        return false;
+    }*/
+
+
+    //Efficient Way
+    int ans=1;
+    for(int i=0;i<=30;i++)
+    {
+        if(ans==n)
+        {
+            return true;
+        }
+        if(ans<INT_MAX/2)
+        ans=ans*2;
+    }
+    return false;
     }
 };
