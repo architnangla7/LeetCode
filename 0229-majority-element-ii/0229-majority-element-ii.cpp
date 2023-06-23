1794677976e9015ff377e68e37a10520a6f167e3
architnangla7 <136116152+architnangla7@users.noreply.github.com>
2023-06-24 02:56:28 +0530

    Time: 15 ms (64.89%), Space: 15.9 MB (69.70%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        //Better Solution by Doing Hashing
+        /*
         vector<int> ls;
         int n=nums.size();
         map<int,int> mpp;
@@ -12,6 +14,44 @@ public:
                 ls.push_back(nums[i]);
             }
         }
+        return ls;
+        */
+        
+        int count1=0;
+        int count2=0;
+        int element1=INT_MIN;
+        int element2=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(count1==0 && element2!=nums[i])
+            {
+                count1=1;
+                element1=nums[i];
+            }
+            else if(count2==0 && element1!=nums[i])
+            {
+                count2=1;
+                element2=nums[i];
+            }
+            else if(element1==nums[i]) count1++;
+            else if(element2==nums[i]) count2++;
+            else
+            {
+                count1--,count2--;
+            }
+        }
+        vector<int> ls;
+        count1=0;
+        count2=0;
+        int mini=(int)(n/3)+1;
+        for(int i=0;i<n;i++)
+        {
+            if(element1==nums[i]) count1++;
+            if(element2==nums[i]) count2++;
+        }
+        if(count1>=mini) ls.push_back(element1);
+        if(count2>=mini) ls.push_back(element2);
         return ls;
     }
 };
