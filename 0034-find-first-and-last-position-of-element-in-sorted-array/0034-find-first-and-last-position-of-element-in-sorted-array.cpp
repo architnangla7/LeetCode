65773b7749042fa5ba74f3cf45839959044b0830
architnangla7 <136116152+architnangla7@users.noreply.github.com>
2023-06-28 17:56:23 +0530

    Time: 12 ms (46.90%), Space: 13.5 MB (74.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //By using concept of lower bound and upperbound
+    /*
     int lowerBound(vector<int> &arr, int x){
     int n=arr.size();
 	int low=0;
@@ -40,7 +42,61 @@ public:
 	}
 	return ans;
 }	
+*/
+    
+    int firstoccurence(vector<int> &nums,int target)
+    {
+        int n=nums.size();
+        int first=-1;
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+            {
+                first=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return first;
+    }
+    int lastoccurence(vector<int> &nums,int target)
+    {
+        int n=nums.size();
+        int last=-1;
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+            {
+                last=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return last;
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
+        /*
         int n=nums.size();
         int lb=lowerBound(nums,target);
         if(lb==n || nums[lb]!=target)
@@ -48,5 +104,10 @@ public:
             return {-1,-1};
         }
         return {lb,upperBound(nums,target)-1};
+        */
+        int first=firstoccurence(nums,target);
+        if(first==-1) return {-1,-1};
+        int last=lastoccurence(nums,target);
+        return {first,last};
     }
 };
