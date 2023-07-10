7ac326cff11898932cb0665d9451e78819c7970e
architnangla7 <136116152+architnangla7@users.noreply.github.com>
2023-07-10 13:15:53 +0530

    Time: 3 ms (45.24%), Space: 8.4 MB (79.60%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,6 +11,8 @@
  */
 class Solution {
 public:
+    //Recursive Post Order Traversal
+    /*
     void postorder(TreeNode* root,vector<int> &ans)
     {
         if(root==NULL) return;
@@ -18,9 +20,35 @@ public:
         postorder(root->right,ans);
         ans.push_back(root->val);
     }
+    */
     vector<int> postorderTraversal(TreeNode* root) {
+        //Recursive Post Order Traversal
+        /*
         vector<int> ans;
         postorder(root,ans);
         return ans;
+        */
+        
+        //Iterative Post Order Traversal Using 2 Stacks
+        vector<int> postorder;
+        if(root==NULL) return postorder;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL){
+                st1.push(root->left);
+            }
+            if(root->right!=NULL){
+                st1.push(root->right);
+            }
+        }
+        while(!st2.empty()){
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
     }
 };
