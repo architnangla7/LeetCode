2e77401c321d29a5aa4c44a94322e69b160440df
architnangla7 <136116152+architnangla7@users.noreply.github.com>
2023-07-26 12:05:01 +0530

    Time: 9 ms (79.26%), Space: 11.6 MB (55.66%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -11,12 +11,13 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+        //
         if(head==NULL || head->next==NULL) return head;
         ListNode* newHead=deleteDuplicates(head->next);
         if(head->val==newHead->val) return newHead;
         else{
             head->next=newHead;
-            return head;
         }
+        return head;
     }
 };
